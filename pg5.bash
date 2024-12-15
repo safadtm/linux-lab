07ccfed78a81d2dfd6a04ebf93ae71532ad5e333
Safad T M <106598178+safadtm@users.noreply.github.com>
2024-12-15 12:01:11 +0530

    Add files via upload

---
+++ pg5.bash
@@ -0,0 +1,13 @@
+echo "enter the numbers"
+read num1
+read num2
+echo "Before Swapping"
+echo "Num1: $num1"
+echo "Num2: $num2"
+num3=$num1
+num1=$num2
+
+num2=$num3
+echo "After Swapping"
+echo "Num1: $num1"
+echo "Num2: $num2"
---
+++ pg6.bash
@@ -0,0 +1,9 @@
+# Dividing 55 by 3 with bc only
+echo "55/3" | bc
+
+# Dividing 55 by 3 with bc with -l option
+echo "55/3" | bc -l
+
+# Dividing 55 by 3 with bc with scale value
+echo "scale=2; 55/3" | bc
+
