c1345d1f80e92a08648e0d1fafa23ed5a2f3513b
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-01-29 07:41:58 +0530

    Update pg10.bash

--- lab1/pg10.bash
+++ lab1/pg10.bash
@@ -1,11 +1,7 @@
 echo -n "Enter the year (yyyy) to find leap year: "
 read year
 
-d=$((year % 4))
-b=$((year % 100))
-c=$((year % 400))
-
-if [ $d -eq 0 -a $b -ne 0 ] || [ $c -eq 0 ]; then
+if (( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )); then
     echo "Year is a leap year"
 else
     echo "Not a leap year"
