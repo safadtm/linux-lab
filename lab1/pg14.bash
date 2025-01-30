968abdbf4dacbcdba938200b2c128d659e477076
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-01-30 18:52:20 +0530

    Update pg14.bash

--- lab1/pg14.bash
+++ lab1/pg14.bash
@@ -1,9 +1,11 @@
-echo "Enter a 7 digit number"
-read num
+echo -n "Enter a 7 digit number to display odd position values"
+read number  # Read the input number
+
 n=1
+
 while [ $n -le 7 ]
 do
-a=`echo $num | cut -c $n`
-echo $a
-n=$(( n+2 ))
+    a=$(echo $number | cut -c $n)
+    echo $a
+    n=$((n+2))
 done
