24714e7911bb87f847be61d3f0cf3f0af42c4964
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-01-30 19:03:19 +0530

    Update pg13.bash

--- lab1/pg13.bash
+++ lab1/pg13.bash
@@ -1,11 +1,14 @@
-echo "Enter a 5 digit number"
-read num
-n=$num
-rev=0
-while [ $num -ne 0 ]
-do
-r=$(( num%10 ))
-rev=$(( rev*10+r ))
-num=$(( num/10 ))
-done
-echo "Reverse of $n is $rev"
+
+echo -n "Enter a number to reverse:"
+read number
+
+n=$number
+reverse=0
+
+while [ $number -ne 0 ]
+    do
+        remainder=$((number%10))
+        reverse=$((reverse*10+remainder))
+        number=$((number/10))
+    done
+echo "Reverse of the $n is $reverse"    
