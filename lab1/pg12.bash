dc77b47119e06af118e426e613bb8a2763af37cb
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-01-30 18:32:09 +0530

    Update pg12.bash

--- lab1/pg12.bash
+++ lab1/pg12.bash
@@ -1,9 +1,11 @@
-echo "Enter a 5 digit number"
-read num
-n=1
-while [ $n -le 5 ]
+echo -n "Enter a 5 digit number to display odd position values: "
+read number  # Read the input number
+
+n=1  # Start from position 1
+
+while [ $n -le 5 ] 
 do
-a=`echo $num | cut -c $n`
-echo $a
-n=$(( n+2 ))
+    a=$(echo $number | cut -c $n) 
+    echo $a
+    n=$(( n + 2 ))  # Increment by 2 to get odd positions (1, 3, 5)
 done
