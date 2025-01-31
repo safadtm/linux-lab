49be251624decabd8ae1015afa874ab65c815733
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-01-31 06:37:45 +0530

    Update pg7.bash

--- lab1/pg7.bash
+++ lab1/pg7.bash
@@ -1,9 +1,12 @@
 echo -n “enter any integer number to find even and odd :-”
 read number
-rem=`expr $number % 2`
-if [ $rem -eq 0 ]
+
+remainder=$((number%2))
+
+if [ $remainder -eq 0 ]
 then
-echo “number is even”
+echo "$number is even"
 else
-echo “number is odd”
+echo "$number is odd"
 fi
+
