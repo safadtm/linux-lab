77089f7e26217c3ddcee51b950e27f00ff523299
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-02-28 10:18:33 +0530

    Update pg18.bash

--- lab2/pg18.bash
+++ lab2/pg18.bash
@@ -1,21 +1,26 @@
-echo "enter the limit"
+# sum of all prime numbers
+echo "Enter the limit"
 read limit
 sum=0
-for (( i=2;i<=$limit;i++ ))
+
+# Check primes from 2 to the given limit
+for (( i=2; i<=limit; i++ ))
 do
-flag=1
-for (( j=2;j<=(i/2);j++ ))
-do
-rem=$(( i%j ))
-if [ $rem -eq 0 ]
-then
-flag=0
-break
-fi
+  flag=1
+  # Only check divisibility up to sqrt(i)
+  for (( j=2; j*j<=i; j++ ))
+  do
+    rem=$(( i % j ))
+    if [ $rem -eq 0 ]; then
+      flag=0
+      break
+    fi
+  done
+  
+  # If the number is prime, add it to the sum
+  if [ $flag -eq 1 ]; then
+    sum=$(( sum + i ))
+  fi
 done
-if [ $flag -eq 1 ]
-then
-sum=$(( sum+i ))
-fi
-done
-echo "sum=$sum"
+
+echo "Sum of prime numbers up to $limit is $sum"
