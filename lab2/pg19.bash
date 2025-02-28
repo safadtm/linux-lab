aad57f7ece69d43351948f509ca98453a42cc95e
Safad T M <106598178+safadtm@users.noreply.github.com>
2025-02-28 10:15:33 +0530

    Update pg19.bash

--- lab2/pg19.bash
+++ lab2/pg19.bash
@@ -1,16 +1,32 @@
-echo "enter two numbers"
+echo "Enter two numbers"
 read num1 num2
-echo "enter the choice"
+
+echo "Enter the choice (addition, subtraction, multiplication, division)"
 read ch
+
 case $ch in
-"addition") sum=$(( num1+num2 ))
-echo "sum is $sum";;
-"subtraction") diff=$(( num1-num2 ))
-echo "difference is $diff";;
-"multiplication") pdt=$(( num1*num2 ))
-echo "product is $pdt";;
-"division") quotient=$(( num1/num2 ))
-echo "quotient is $quotient";;
-*)
-echo "wrong choice"
+  addition)
+    sum=$(( num1 + num2 ))
+    echo "Sum is $sum"
+    ;;
+  subtraction)
+    diff=$(( num1 - num2 ))
+    echo "Difference is $diff"
+    ;;
+  multiplication)
+    pdt=$(( num1 * num2 ))
+    echo "Product is $pdt"
+    ;;
+  division)
+    # Check for division by zero
+    if [ $num2 -eq 0 ]; then
+      echo "Error: Division by zero is not allowed."
+    else
+      quotient=$(( num1 / num2 ))
+      echo "Quotient is $quotient"
+    fi
+    ;;
+  *)
+    echo "Wrong choice"
+    ;;
 esac
